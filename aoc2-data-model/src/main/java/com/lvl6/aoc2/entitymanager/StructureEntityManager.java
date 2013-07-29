1561a1dc5684316f02af65d5f1c8afa7667ad6e2
Byron Tang <byronyugontang@gmail.com>
2013-07-29 12:07:22 -0700

    entitymanagers

---
+++ aoc2-data-model/src/main/java/com/lvl6/aoc2/entitymanager/StructureEntityManager.java
@@ -0,0 +1,34 @@
+package com.lvl6.aoc2.entitymanager;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+import com.google.common.collect.ImmutableMap;
+import com.lvl6.aoc2.po.Structure;
+
+@Component
+public class StructureEntityManager extends BaseEntityManager<Structure, UUID>{
+
+	
+	
+	
+	
+	public StructureEntityManager() {
+		super(Structure.class, UUID.class);
+	}
+
+
+	@Override
+	protected ImmutableMap<String, Object> getIndexes() {
+		return ImmutableMap.<String, Object>builder()
+			.put("column_metadata", ImmutableMap.<String, Object>builder()
+	            //.put("userName", getIndexProps("userName"))
+	            //.put("email", getIndexProps("email"))
+	         .build())
+	    .build();
+	}
+	
+
+
+}
---
+++ aoc2-data-model/src/main/java/com/lvl6/aoc2/entitymanager/UserStructureEntityManager.java
@@ -0,0 +1,34 @@
+package com.lvl6.aoc2.entitymanager;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+import com.google.common.collect.ImmutableMap;
+import com.lvl6.aoc2.po.UserStructure;
+
+@Component
+public class UserStructureEntityManager extends BaseEntityManager<UserStructure, UUID>{
+
+	
+	
+	
+	
+	public UserStructureEntityManager() {
+		super(UserStructure.class, UUID.class);
+	}
+
+
+	@Override
+	protected ImmutableMap<String, Object> getIndexes() {
+		return ImmutableMap.<String, Object>builder()
+			.put("column_metadata", ImmutableMap.<String, Object>builder()
+	            //.put("userName", getIndexProps("userName"))
+	            //.put("email", getIndexProps("email"))
+	         .build())
+	    .build();
+	}
+	
+
+
+}
