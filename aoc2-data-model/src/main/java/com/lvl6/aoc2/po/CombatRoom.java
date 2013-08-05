c7fa3a0771d4a83403cc2974d1458cdef27c4e34
Arturo Wu Zhou <aweezy90@gmail.com>
2013-08-05 15:13:52 -0700

    removed overriding  hashCode() and equals()

--- aoc2-data-model/src/main/java/com/lvl6/aoc2/po/CombatRoom.java
+++ aoc2-data-model/src/main/java/com/lvl6/aoc2/po/CombatRoom.java
@@ -124,55 +124,6 @@ public class CombatRoom extends BasePersistentObject{
 	}
 
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + lvlRequired;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + order;
-		result = prime * result + timeMillisOne;
-		result = prime * result + timeMillisThree;
-		result = prime * result + timeMillisTwo;
-		result = prime * result + type;
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		CombatRoom other = (CombatRoom) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (lvlRequired != other.lvlRequired)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (order != other.order)
-			return false;
-		if (timeMillisOne != other.timeMillisOne)
-			return false;
-		if (timeMillisThree != other.timeMillisThree)
-			return false;
-		if (timeMillisTwo != other.timeMillisTwo)
-			return false;
-		if (type != other.type)
-			return false;
-		return true;
-	}
 
 
 	@Override
--- aoc2-data-model/src/main/java/com/lvl6/aoc2/po/Monster.java
+++ aoc2-data-model/src/main/java/com/lvl6/aoc2/po/Monster.java
@@ -159,64 +159,6 @@ public class Monster extends BasePersistentObject{
 	}
 
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + attack;
-		result = prime * result + color;
-		result = prime * result + defense;
-		result = prime * result + expReward;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + (isBoss ? 1231 : 1237);
-		result = prime * result + maxHp;
-		result = prime * result + maxMana;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + size;
-		result = prime * result + type;
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Monster other = (Monster) obj;
-		if (attack != other.attack)
-			return false;
-		if (color != other.color)
-			return false;
-		if (defense != other.defense)
-			return false;
-		if (expReward != other.expReward)
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (isBoss != other.isBoss)
-			return false;
-		if (maxHp != other.maxHp)
-			return false;
-		if (maxMana != other.maxMana)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (size != other.size)
-			return false;
-		if (type != other.type)
-			return false;
-		return true;
-	}
 
 
 	@Override
