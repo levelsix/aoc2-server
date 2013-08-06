c9f0a5040624e8a07be15c0acf1ba25ab01e2516
Arturo Wu Zhou <aweezy90@gmail.com>
2013-08-05 19:29:47 -0700

    unfinished repair equip event

---
+++ aoc2-services/src/main/java/com/lvl6/aoc2/services/userequiprepair/UserEquipRepairService.java
@@ -0,0 +1,35 @@
+package com.lvl6.aoc2.services.userequiprepair;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import com.lvl6.aoc2.entitymanager.UserEquipRepairEntityManager;
+import com.lvl6.aoc2.po.UserEquip;
+import com.lvl6.aoc2.po.UserEquipRepair;
+
+public interface UserEquipRepairService {
+	
+	public abstract Map<UUID, UserEquipRepair> getEquipsBeingRepaired(String userIdString);
+	
+	//returns AocTwoTableConstants.resourceType -> amount
+	public abstract Map<Integer, Integer> calculateRefund(List<UserEquipRepair> refundList);
+	
+	public abstract Map<Integer, Integer> calculateRepairCost(List<UserEquip> ueList);
+	
+	public abstract void deleteUserEquipRepairs(Collection<UUID> ids);
+	
+	public abstract void saveUserEquipRepairs(Collection<UserEquipRepair> newStuff);
+	
+	
+	
+	
+	
+	
+	
+	public abstract UserEquipRepairEntityManager getUserEquipmentRepairEntityManager();
+	
+	public abstract void setUserEquipmentRepairEntityManager(UserEquipRepairEntityManager userEquipRepairEntityManager);
+
+}
